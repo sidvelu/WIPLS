661f323cbf4ed27b4559046060224c27e5b215de
root <root@beaglebone.(none)>
2014-04-23 20:22:17 +0000

    Changes to PanTilt pins

--- PanTilt.py
+++ PanTilt.py
@@ -3,11 +3,10 @@ import Adafruit_BBIO.GPIO as GPIO
 import time
 import sys
 
-upPin = "P8_16"
-downPin = "P8_14"
-leftPin = "P8_26"
-rightPin = "P8_10"
-
+upPin = "P8_14"
+downPin = "P8_18"
+leftPin = "P8_12"
+rightPin = "P8_16"
 
 class PanTilt:
     def __init__(self):
@@ -19,36 +18,36 @@ class PanTilt:
 
     #Toggling relays for pan and tilt
     def left(self):
-        GPIO.output(leftPin,GPIO.LOW)
-        GPIO.output(rightPin,GPIO.HIGH)
-        GPIO.output(upPin,GPIO.HIGH)
-        GPIO.output(downPin,GPIO.HIGH)
+        GPIO.output(leftPin,GPIO.HIGH)
+        GPIO.output(rightPin,GPIO.LOW)
+        GPIO.output(upPin,GPIO.LOW)
+        GPIO.output(downPin,GPIO.LOW)
         print ("Panning Left")
 		
     def right(self):
-        GPIO.output(leftPin,GPIO.HIGH)
-        GPIO.output(rightPin,GPIO.LOW)
-        GPIO.output(upPin,GPIO.HIGH)
-        GPIO.output(downPin,GPIO.HIGH)
+        GPIO.output(leftPin,GPIO.LOW)
+        GPIO.output(rightPin,GPIO.HIGH)
+        GPIO.output(upPin,GPIO.LOW)
+        GPIO.output(downPin,GPIO.LOW)
         print ("Panning Right")
 		
     def up(self):
-        GPIO.output(leftPin,GPIO.HIGH)
-        GPIO.output(rightPin,GPIO.HIGH)
-        GPIO.output(upPin,GPIO.LOW)
-        GPIO.output(downPin,GPIO.HIGH)
+        GPIO.output(leftPin,GPIO.LOW)
+        GPIO.output(rightPin,GPIO.LOW)
+        GPIO.output(upPin,GPIO.HIGH)
+        GPIO.output(downPin,GPIO.LOW)
         print ("Tilting Up")
 		
     def down(self):
-        GPIO.output(leftPin,GPIO.HIGH)
-        GPIO.output(rightPin,GPIO.HIGH)
-        GPIO.output(upPin,GPIO.HIGH)
-        GPIO.output(downPin,GPIO.LOW)
+        GPIO.output(leftPin,GPIO.LOW)
+        GPIO.output(rightPin,GPIO.LOW)
+        GPIO.output(upPin,GPIO.LOW)
+        GPIO.output(downPin,GPIO.HIGH)
         print ("Tilting Down")
 		
     def stop(self):
-        GPIO.output(leftPin,GPIO.HIGH)
-        GPIO.output(rightPin,GPIO.HIGH)
-        GPIO.output(upPin,GPIO.HIGH)
-        GPIO.output(downPin,GPIO.HIGH)
+        GPIO.output(leftPin,GPIO.LOW)
+        GPIO.output(rightPin,GPIO.LOW)
+        GPIO.output(upPin,GPIO.LOW)
+        GPIO.output(downPin,GPIO.LOW)
         print ("Motion Stop")
