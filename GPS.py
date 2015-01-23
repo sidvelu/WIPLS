7bc560474058dc0d1b583b4a396acbdf8ea6d9b6
Siddharth Velu <sidvelu@gmail.com>
2015-01-23 15:40:19 -0500

    GPS script added

---
+++ GPS.py
@@ -0,0 +1,39 @@
+import Adafruit_BBIO.UART as UART
+import serial
+import time
+import datetime as dt
+
+
+class GPS:
+
+    ser = None
+
+    def __init__(self):
+        UART.setup("UART1")
+        ser = serial.Serial(port = "/dev/ttyO1", baudrate=9600, timeout=1, writeTimeout=1)
+        ser.close()
+        ser.open()
+
+        if ser.isOpen():
+            print "Serial is open!"
+
+    def getHeading(self):
+        fix = False
+        start = dt.datetime.now()
+        while (fix == False and (end-start).microseconds < 1000):
+            line = ser.readline()
+            if "$GPGGA" in line:
+                line = line.split(",") # return list split by comma
+                message_ID    = line[0]  # message ID
+                UTC_time      = line[1]  # UTC time
+                latitude_num  = line[2]  # latitude (ddmm.mmmm)
+                latitude_dir  = line[3]  # latitude direction (N or S)
+                longitude_num = line[4]  # longitude (dddmm.mmmm)
+                longitude_dir = line[5]  # longitude direction (E or W)
+                print latitude_num + latitude_dir
+                print longitude_num + longitude_dir
+                if latitude_num != "" and latitude_dir != "":
+                    fix = True
+            end = dt.datetime.now()
+        if(fix == False)
+            print "Fix Not Available"
