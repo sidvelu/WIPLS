a7ccba989fe7a46ce38604786afc9bd8ba14852e
butane- <tan.be@husky.neu.edu>
2015-04-06 19:37:36 -0400

    Added scrollbar to errorbox and changed it to a div
    Also, error messages insert from the top now so the most recent ones
    will show without scrolling

--- Map/templates/MapOverlayScript.js
+++ Map/templates/MapOverlayScript.js
@@ -17,13 +17,21 @@
 //    document.body.appendChild(form);
 //    form.submit();
 //}
-        
+
+var prevMessage = null;
+
 function printmessage(message) {
     var node = document.createElement("li");
     var textnode = document.createTextNode(message);
     node.appendChild(textnode);
     
-    document.getElementById("errorbox").appendChild(node);
+    if (prevMessage == null) {
+        document.getElementById("errorbox").appendChild(node);
+    } else {
+        document.getElementById("errorbox").insertBefore(node,
+                                                prevMessage);
+    }
+    prevMessage = node;
 }
 
 function arrow(up) {
